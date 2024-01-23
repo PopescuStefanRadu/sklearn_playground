3aaeb41c211f23d1c2f66cdc36d3f676822dea22
Popescu Stefan Radu <popescustefanradu@gmail.com>
2024-01-24 00:08:57 +0200

    Linear regression example

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import linear_model
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    raw_data = pd.read_csv("GlobalTemperatures.csv", parse_dates=['dt'])
+    date_and_avg_temp = raw_data[['dt', 'LandAverageTemperature']]
+    print(date_and_avg_temp)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    plt.scatter(date_and_avg_temp['dt'], date_and_avg_temp['LandAverageTemperature'], color='red')
+    plt.title("Temp by date")
+    plt.show()
+
+    avg_temp_by_year = date_and_avg_temp.groupby(date_and_avg_temp['dt'].dt.year)[
+        'LandAverageTemperature'].mean().reset_index()
+    print(avg_temp_by_year)
+
+    plt.scatter(avg_temp_by_year['dt'], avg_temp_by_year['LandAverageTemperature'], color='blue')
+    plt.title("Temp by year")
+    plt.show()
+
+    regression = linear_model.LinearRegression()
+
+    regression.fit(avg_temp_by_year['dt'].values.reshape(-1, 1), avg_temp_by_year['LandAverageTemperature'])
+    # prediction = regression.predict([[2023], [2024], [2025], [2026], [2027], [2028]])
+    prediction = regression.predict(avg_temp_by_year['dt'].values.reshape(-1, 1))
+    plt.scatter(avg_temp_by_year['dt'], avg_temp_by_year['LandAverageTemperature'], color='blue')
+    plt.plot(avg_temp_by_year['dt'], prediction, color='red')
+    plt.title("Prediction vs data")
+    plt.show()
